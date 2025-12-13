2a05e6b3838d0e9697eabfdf46832f3aa835baf5
Pratik Ajbe <124169308+PratikAjbe01@users.noreply.github.com>
2025-12-13 16:19:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (15.29%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     int n=s.size();
+   unordered_map<char, char> mp = {
+    {')', '('},
+    {']', '['},
+    {'}', '{'}
+};
+
+     stack<char>st;
+     for(int i=0;i<n;i++){
+        if(s[i]==')'||s[i]==']'||s[i]=='}'){
+            char temp=mp[s[i]];
+            if(!st.empty()&&st.top()==temp){
+                st.pop();
+            }
+            else return false;
+        }else{
+            st.push(s[i]);
+        }
+     }
+     if(!st.empty())return false;
+     return true;
+    }
+};
