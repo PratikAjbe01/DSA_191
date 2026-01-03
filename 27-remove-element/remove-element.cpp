498fe6612a0586d9fb09ef8d4bc1729a3071ce84
Pratik Ajbe <124169308+PratikAjbe01@users.noreply.github.com>
2026-01-03 19:55:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (46.90%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+     
+        vector<int>temp;
+        for(int i=0;i<n;i++){
+            if(nums[i]==val)continue;
+            else temp.push_back(nums[i]);
+        }
+    for(int i=0;i<temp.size();i++)nums[i]=temp[i];
+        return temp.size();
+    }
+};
