aef64a3fa3d50cb546e0ea4911e4f9a15ed70e72
Pratik Ajbe <124169308+PratikAjbe01@users.noreply.github.com>
2026-01-20 10:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-sort(s.begin(),s.end());
-sort(t.begin(),t.end());
-return s==t;
+    int n1=s.size();
+    int n2=t.size();
+    if(n1!=n2)return false;
+    vector<int>mp(26,0);
+    for(int i=0;i<n1;i++){
+        mp[s[i]-'a']++;
+    }
+    for(int i=0;i<n2;i++){
+        if(mp[t[i]-'a']!=0){
+            mp[t[i]-'a']--;
+        }else{
+            mp[t[i]-'a']++;
+        }
+    } 
+    for(int i=0;i<mp.size();i++){
+        if(mp[i]!=0)return false;
+    }   
+    return true;
     }
 };
